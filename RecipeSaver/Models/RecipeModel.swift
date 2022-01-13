d327bacdb8ed0dc9e2abe3738c83d1e88c546d93
ron <ron.hsien.chang@gmail.com>
2022-01-13 23:08:55 +0400

    Add RecipeCard and RecipeList componets

--- RecipeSaver/Models/RecipeModel.swift
+++ RecipeSaver/Models/RecipeModel.swift
@@ -30,3 +30,28 @@ struct Recipe: Identifiable {
     let datePublished: String
     let url: String
 }
+
+extension Recipe {
+    static let all: [Recipe] = [
+        Recipe(
+            name: "Cream Carrot Soup",
+            image: "https://www.forksoverknives.com/wp-content/uploads/fly-images/98892/Creamy-Carrot-Soup-for-Wordpress-360x270-c.jpg",
+            description: "This bold-hued soup is perfectly sweet and seriously comforting. Peeling the red bell pepper helps make this carrot soup extra silky. Dress it up with a drizzle of rich cashew cream, pomegranate seeds, and chopped fresh parsley for a festive fall presentation.",
+            ingredients: "¼ cup cashews\n1 medium onion, cut into ½-inch dice (2 cups)\n1½ pounds carrots, cut into ½-inch dice (3½ cups)\n1 red bell pepper, peeled and cut into ½-inch dice (1 cup)\n1 sprig fresh rosemary\n1 sprig fresh thyme\n1 large fresh sage leaf\n4 cloves garlic, minced\n1 tablespoon lemon juice\nSea salt and freshly ground black pepper or cayenne pepper, to taste\n½ cup fresh pomegranate seeds\n2 tablespoons finely chopped fresh parsley",
+            directions: "In a small bowl, cover the cashews with ½ cup hot water; let soak 20 minutes. Transfer cashews and remaining liquid to a blender; blend until smooth and creamy. Set aside.\n In a saucepan, combine onion, carrots, bell pepper, rosemary, thyme, sage, and garlic. Add ½ cup water and sauté over medium heat for 15 to 20 minutes, or until carrots are tender.\n Remove pan from heat. Let vegetables cool for 10 minutes; then remove rosemary sprigs and thyme leaf.\n Transfer sauteed vegetables to a blender, and puree to a smooth paste. Return pureed vegetables to saucepan and add 3 cups water and 1 tablespoon lemon juice. Season with salt and pepper. Bring soup to boiling.\n To serve, place soup in individual bowls, drizzle with cashew cream, and garnish with pomegranate seeds and parsley.",
+            category: "Soup",
+            datePublished: "2019-11-11",
+            url: "https://www.forksoverknives.com/recipes/vegan-soups-stews/creamy-carrot-soup/"
+        ),
+        Recipe(
+            name: "Kale, Apple, and Quinoa Salad",
+            image: "https://www.forksoverknives.com/wp-content/uploads/fly-images/98341/Kale-Apple-Quinoa-Salad-wordpress-360x270-c.jpg",
+            description: "Each bite of this colorful salad is filled with the flavors of favorite fall ingredients such as apples, butternut squash, and pumpkin seeds. For optimum freshness and texture, wait to add the dressing until just before serving.\n Note that this recipe calls for cooked quinoa, so have that ready to go: For 1½ cups cooked quinoa, rinse and drain ½ cup quinoa; cook according to package directions; and drain if needed. Check out our Grains Cooking Guide for more tips on prepping grains like a pro.",
+            ingredients: "2 cups ¾-inch pieces peeled butternut squash\n ¼ cup pumpkin seeds\n ¼ teaspoon ground cinnamon\n 1 lemon\n 2 tablespoons pure maple syrup\n 1 tablespoon Dijon mustard\n 3 cups stemmed and chopped kale\n 1½ cups cooked quinoa\n 1 cup chopped apple\n ½ cup sliced green onions\n Sea salt and freshly ground black pepper, to taste",
+            directions: "Preheat oven to 400°F. Line a 15×10-inch baking pan with parchment paper or a silicone baking mat. Arrange squash in the prepared baking pan. Roast 20 to 25 minutes or until squash is tender, brushing lightly with water if squash starts to look dry. Cool slightly on a wire rack.\n Meanwhile, in a small bowl toss together pumpkin seeds and ½ teaspoon water; sprinkle with cinnamon. Place pumpkin seeds in a small, shallow baking pan. Roast 5 minutes or until lightly toasted; cool.\n For dressing, remove 1 teaspoon zest and squeeze 2 tablespoons juice from lemon. In a small bowl whisk together lemon zest and juice, maple syrup, and mustard.\n In a large bowl combine kale, quinoa, apple, green onions, and roasted squash. Add dressing; toss to coat. Season with salt and pepper. Top with pumpkin seeds.",
+            category: "Salad",
+            datePublished: "2019-10-22",
+            url: "https://www.forksoverknives.com/recipes/vegan-salads-sides/kale-apple-and-quinoa-salad/"
+        )
+    ]
+}
