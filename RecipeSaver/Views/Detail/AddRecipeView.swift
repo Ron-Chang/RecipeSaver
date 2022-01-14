cfe3e5d5f19bee1d267d79ef50b3c93be72ddbee
ron <ron.hsien.chang@gmail.com>
2022-01-14 22:18:45 +0400

    Add gitignore

---
+++ RecipeSaver/Views/Detail/AddRecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  AddRecipeView.swift
+//  RecipeSaver
+//
+//  Created by Ron Chang on 2022/1/14.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRecipeView()
+    }
+}
