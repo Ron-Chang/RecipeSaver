90bfd457f7e218690521c71a4b39994a5250ddf2
ron <ron.hsien.chang@gmail.com>
2022-01-14 22:19:37 +0400

    Add AddRecipeView to add new recipes

--- RecipeSaver/Views/Detail/AddRecipeView.swift
+++ RecipeSaver/Views/Detail/AddRecipeView.swift
@@ -8,8 +8,75 @@
 import SwiftUI
 
 struct AddRecipeView: View {
+    
+    @State private var name: String = ""
+    @State private var selectCategory: Category = Category.main
+    @State private var description: String = ""
+    @State private var ingredients: String = ""
+    @State private var directions: String = ""
+    @State private var navigateToRecipe = false
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("Name")) {
+                    TextField("Recipe Name", text: $name)
+                }
+                
+                Section(header: Text("Category")) {
+                    Picker("Category", selection: $selectCategory) {
+                        ForEach(Category.allCases) { category in
+                            Text(category.rawValue)
+                                .tag(category)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }
+                
+                Section(header: Text("Description")) {
+                    TextEditor(text: $description)
+                }
+                
+                Section(header: Text("Ingredients")) {
+                    TextEditor(text: $ingredients)
+                }
+                
+                Section(header: Text("Directions")) {
+                    TextEditor(text: $directions)
+                }
+            }
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Label("Cancel", systemImage: "xmark")
+                            .labelStyle(.iconOnly)
+                    }
+                }
+                
+                ToolbarItem {
+
+                    NavigationLink(isActive: $navigateToRecipe) {
+                        RecipeView(recipe: Recipe.all.sorted{ $0.datePublished > $1.datePublished }[0])
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        Button {
+                            navigateToRecipe = true
+                        } label: {
+                            Label("Done", systemImage: "checkmark")
+                                .labelStyle(.iconOnly)
+                        }
+                    }
+                    .disabled(name.isEmpty)
+                }
+            })
+            .navigationTitle("New Recipe")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
